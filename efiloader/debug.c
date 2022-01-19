dfa37ce801a28be40580bf3e1a09bd65da89f95c
Joseph Charamut <12399761+joecharamut@users.noreply.github.com>
2022-01-18 22:01:43 -0500

    rename serial port

--- efiloader/debug.c
+++ efiloader/debug.c
@@ -13,32 +13,32 @@ uint8_t inb(uint16_t port) {
     return value;
 }
 
-#define COM0_PORT 0x3f8
+const uint16_t SERIAL_PORT = 0x3f8;
 
 int serial_init() {
-    outb(COM0_PORT + 1, 0x00);    // Disable all interrupts
-    outb(COM0_PORT + 3, 0x80);    // Enable DLAB (set baud rate divisor)
-    outb(COM0_PORT + 0, 0x03);    // Set divisor to 3 (lo byte) 38400 baud
-    outb(COM0_PORT + 1, 0x00);    //                  (hi byte)
-    outb(COM0_PORT + 3, 0x03);    // 8 bits, no parity, one stop bit
-    outb(COM0_PORT + 2, 0xC7);    // Enable FIFO, clear them, with 14-byte threshold
-    outb(COM0_PORT + 4, 0x0B);    // IRQs enabled, RTS/DSR set
-    outb(COM0_PORT + 4, 0x1E);    // Set in loopback mode, test the serial chip
-    outb(COM0_PORT + 0, 0xAE);    // Test serial chip (send byte 0xAE and check if serial returns same byte)
+    outb(SERIAL_PORT + 1, 0x00);    // Disable all interrupts
+    outb(SERIAL_PORT + 3, 0x80);    // Enable DLAB (set baud rate divisor)
+    outb(SERIAL_PORT + 0, 0x03);    // Set divisor to 3 (lo byte) 38400 baud
+    outb(SERIAL_PORT + 1, 0x00);    //                  (hi byte)
+    outb(SERIAL_PORT + 3, 0x03);    // 8 bits, no parity, one stop bit
+    outb(SERIAL_PORT + 2, 0xC7);    // Enable FIFO, clear them, with 14-byte threshold
+    outb(SERIAL_PORT + 4, 0x0B);    // IRQs enabled, RTS/DSR set
+    outb(SERIAL_PORT + 4, 0x1E);    // Set in loopback mode, test the serial chip
+    outb(SERIAL_PORT + 0, 0xAE);    // Test serial chip (send byte 0xAE and check if serial returns same byte)
 
     // Check if serial is faulty (i.e: not same byte as sent)
-    if(inb(COM0_PORT + 0) != 0xAE) {
+    if(inb(SERIAL_PORT + 0) != 0xAE) {
         return 1;
     }
 
     // If serial is not faulty set it in normal operation mode
     // (not-loopback with IRQs enabled and OUT#1 and OUT#2 bits enabled)
-    outb(COM0_PORT + 4, 0x0F);
+    outb(SERIAL_PORT + 4, 0x0F);
     return 0;
 }
 
 void serial_write(char c) {
-    outb(COM0_PORT, c);
+    outb(SERIAL_PORT, c);
 }
 
 static void emit_char(char c) {

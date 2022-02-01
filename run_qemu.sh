04481d001978a7d1c13dd1e879a9b0de7af943c0
Joseph Charamut <12399761+joecharamut@users.noreply.github.com>
2022-02-01 12:11:55 -0500

    clean up the command

--- run_qemu.sh
+++ run_qemu.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/env bash
 
-qemu-system-x86_64 \
-            -s \
-            -enable-kvm \
-            -no-shutdown \
-            -no-reboot \
-            -d cpu_reset,int,mmu,pcall \
-            -serial file:/dev/stdout \
-            -m 2G \
-            -vga std \
-            -net none \
-            -audiodev id=pa,driver=pa \
-            -drive if=pflash,format=raw,unit=0,readonly=on,file=OVMF_CODE.fd \
-            -drive if=pflash,format=raw,unit=1,file=OVMF_VARS.fd \
-            -drive file=disk.img,format=raw
+ARGS=(
+  -s                        # enable gdb connection on 127.0.0.1:1234
+  -enable-kvm               # enable kernel virtual machine support
+  -no-shutdown              # pause on shutdown
+  -no-reboot                # pause on reboot
+  -d cpu_reset              # dump regs on reset
+  -d int                    # interrupts
+  -d mmu                    # mmu operations
+  -d pcall                  # protected mode calls
+  -serial file:/dev/stdout  # redirect serial to stdout
+  -m 128M                   # 128mb ram
+  -vga std                  # standard vga card
+  -net none                 # no network yet
+  -audiodev pa,id=pa1,server=/run/user/$UID/pulse/native            # pulseaudio support
+  -drive if=pflash,format=raw,unit=0,readonly=on,file=OVMF_CODE.fd  # UEFI firmware
+  -drive if=pflash,format=raw,unit=1,file=OVMF_VARS.fd              # UEFI variables
+  -drive format=raw,file=disk.img                                   # hard drive
+)
+
+echo "Running QEMU: qemu-system-x86_64 ${ARGS[@]}"
+qemu-system-x86_64 ${ARGS[@]}
